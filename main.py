40be0370e121191b04ec3faa703e552d353bea47
Cardboard07 <hrishikesh.sanipini@gmail.com>
2026-02-03 09:12:31 +0530

    isolate main for pip freeze debug

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import subprocess
+import sys
 
-st.title("DEBUG MODE: pip freeze")
+st.title("DEBUG MODE: python -m pip freeze")
 
-out = subprocess.check_output(["pip", "freeze"]).decode()
+out = subprocess.check_output(
+    [sys.executable, "-m", "pip", "freeze"]
+).decode()
+
 st.code(out)
-
 st.stop()
