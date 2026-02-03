dec342e6989b8ad5cdb6ccd380dcecc449aa518f
Cardboard07 <hrishikesh.sanipini@gmail.com>
2026-02-03 09:14:41 +0530

    isolate main for pip freeze debug

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 import streamlit as st
-import subprocess
-import sys
+import pandas as pd
+from pipeline import parse_query
+import io
+buffer = io.BytesIO()
+st.title('NLP query extraction')
+st.write('choose doc uploader or text input')
+choice = st.radio("How would you like to provide data?", ["Text Input", "File Upload"])
+rows = []
+if(choice=='File Upload'):
+    input=st.file_uploader('upload file here',type='txt')
+    if input is not None:
+        stringio = io.StringIO(input.getvalue().decode("utf-8"))
+        for line in stringio:
+            clean_line = line.strip()
+            if clean_line:
+                result = parse_query(clean_line)
+                row = {"query": clean_line}
+                row.update(result)
+                rows.append(row)
+        df = pd.DataFrame(rows)
+else:
+    input=st.text_input('query')
+    result = parse_query(input)
+    row = {"query": input}
+    row.update(result)
+    rows.append(row)
 
-st.title("DEBUG MODE: python -m pip freeze")
-
-out = subprocess.check_output(
-    [sys.executable, "-m", "pip", "freeze"]
-).decode()
-
-st.code(out)
-st.stop()
+df = pd.DataFrame(rows)
+st.write(df)
+df.to_excel("my_data.xlsx", index=False)
+st.download_button(
+    label="Download excel",
+    data=buffer.getvalue(),
+    file_name="my_data.xlsx",
+    mime="text/csv",
+    icon=":material/download:",
+)
